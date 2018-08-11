f566d31cbc223de595eb163bd599b8255e02adf6
Syed Faizan <smfai200@gmail.com>
2018-08-12 00:00:30 +0500

    getter and setters

---
+++ 6. Getter and Setter/Custom Getter and Setter.kt
@@ -0,0 +1,22 @@
+class UserProfilegettersetter(var Name:String = ""){
+    var Name1:String = ""
+
+    fun get_Name1():String{
+        return this.Name1
+    }
+
+    fun set_Name1(Name1:String){
+        this.Name1 = Name1
+    }
+}
+
+
+//fun main(args : Array<String>) {
+//    val User1 = UserProfilegettersetter()
+//    User1.set_Name1("TestCustom")
+//    print(User1.get_Name1())
+//}
+
+
+
+
---
+++ 6. Getter and Setter/Default Getter and Setter.kt
@@ -0,0 +1,16 @@
+class UserProfilegettersetter(var Name:String = ""){
+    var Name1:String
+        get() = this.Name
+        set(value) {
+            this.Name = value
+        }
+}
+
+//fun main(args : Array<String>) {
+//    val User1 = UserProfilegettersetter()
+//    User1.Name1 = "TestOne"
+//    print(User1.Name1)
+//}
+
+
+
